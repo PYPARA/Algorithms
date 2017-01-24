278856578941c6dbd2fe1e261e97a4294f6edd64
Pypara <pyparaloveslife@gmail.com>
2017-01-24 22:32:12 +0800

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,3 +20,16 @@ int main() {
 
 	return 0;
 }
+
+
+/*
+Pseudocode：
+INSERTION-SORT (A, n)⊳A[1 . . n]
+    for j ←2 to n 
+        do  key ←A[ j]
+            i ←j –1
+            while  i > 0 and  A[i] > key
+                do A[i+1] ←A[i]
+                    i ←i –1
+            A[i+1] = key
+*/
