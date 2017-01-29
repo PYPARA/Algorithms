0b18711def51f368b4d5bf66f4fcf6fb6e89a526
Pypara <pyparaloveslife@gmail.com>
2017-01-29 15:28:02 +0800

    Add files via upload

---
+++ Powering a number.cpp
@@ -0,0 +1,35 @@
+﻿/*
+Powering a number
+Problem:Compute an, where n ∈N.
+Naive algorithm:Θ(n).
+an=an/2 ⋅an/2                 if n is even;
+  =a(n–1)/2 ⋅a(n–1)/2 ⋅a    if n is odd.
+   Divide-and-conquer algorithm:T(n) = T(n/2) + Θ(1) 
+									⇒T(n) = Θ(lgn).
+*/
+#include<iostream>
+int Powering_a_number(int a, int n) {
+	if (0 == n) {
+		return 1;
+	}
+	if (1 == n) {
+		return a;
+	}
+	if (2 == n) {
+		return a*a;
+	}
+	bool p = (n % 2) ? true : false;
+	if (p) {
+	return Powering_a_number(Powering_a_number(a, (n - 1) / 2), 2)*a;
+	}
+	else {
+		return Powering_a_number(Powering_a_number(a, n / 2), 2);
+	}
+}
+
+int main() {
+	int an = Powering_a_number(2, 10);
+	std::cout << an << std::endl;
+
+	return 0;
+}
